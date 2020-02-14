958bd5b0dfbc35058ec77526f0b3f87f293d3694
Igor.oh <ogayigore1996@mail.ru>
2020-02-15 00:05:41 +0300

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Игорь Огай on 15.02.2020.
+//  Copyright © 2020 Игорь Огай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
